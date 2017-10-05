b3f26e60d0e78cbc920caea4e33d5e1c6d4bda89
Jostein Dyrseth <josdyr@Josteins-MacBook-Pro.local>
2017-10-05 11:27:51 +0100

    This is my first commit

---
+++ redirect_and_errors.py
@@ -0,0 +1,13 @@
+from flask import Flask, redirect, url_for
+app = Flask(__name__)
+
+@app.route("/private")
+def private():
+    return redirect(url_for('login'))
+
+@app.route("/login")
+def login():
+    return "Here we would get username and password!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
